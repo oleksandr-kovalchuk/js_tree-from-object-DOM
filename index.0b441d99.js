9b0772978071cafa713747c83f95598358c5be85
Oleksandr Kovalchuk <kov280396@gmail.com>
2024-09-28 09:26:05 +0300

    make build

---
+++ index.0b441d99.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in n.append(r),t){var c=document.createElement("li");c.textContent=a,Object.keys(t[a]).length&&e(c,t[a]),r.append(c)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.0b441d99.js.map
